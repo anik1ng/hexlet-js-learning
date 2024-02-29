5fead017691f189da949135677f57d23a4f7a65a
Constantine Anikin <mail@anikin.pro>
2024-02-29 12:49:18 -0500

    Update strings.js

--- arrays/strings.js
+++ arrays/strings.js
@@ -101,3 +101,55 @@ const countUniqChars = (string) => {
 };
 
 export default countUniqChars;
+
+
+// Реализуйте и экспортируйте функцию по умолчанию, которая принимает на вход строку, 
+// состоящую только из открывающих и закрывающих скобок разных типов, и проверяет, является ли эта строка сбалансированной. 
+// Открывающие и закрывающие скобки должны быть одного вида. Пустая строка (отсутствие скобок) считается сбалансированной.
+
+// Строка считается корректной (сбалансированной), если содержащаяся в ней скобочная структура соответствует требованиям:
+
+// Скобки — это парные структуры. У каждой открывающей скобки должна быть соответствующая ей закрывающая скобка.
+// Скобки должны закрываться в правильном порядке:
+// import isBracketStructureBalanced from './strings.js';
+ 
+// // Пример вложенности
+// isBracketStructureBalanced('(>');  // false
+// isBracketStructureBalanced('()');  // true
+// isBracketStructureBalanced('[()]');  // true
+// isBracketStructureBalanced('({}[])');  // true
+// isBracketStructureBalanced('{<>}}'); // false
+// isBracketStructureBalanced('([)]'); // false
+// Функция должна поддерживать, минимум, четыре вида скобок: круглые — (), квадратные — [], фигурные — {} и угловые — <>.
+
+const openingSymbols = ['(', '[', '{', '<'];
+const closingSymbols = [')', ']', '}', '>'];
+
+const isBracketStructureBalanced = (string) => {
+  if (string === '') {
+    return true;
+  }
+
+  const stack = [];
+
+  for (const symbol of string) {
+    const openingIndex = openingSymbols.indexOf(symbol);
+    const closingIndex = closingSymbols.indexOf(symbol);
+
+    if (openingIndex !== -1) {
+      stack.push(openingIndex);
+    } else {
+      if (closingIndex === -1) {
+        return false;
+      }
+
+      if (stack.length === 0 || stack.pop() !== closingIndex) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
+};
+
+export default isBracketStructureBalanced;
