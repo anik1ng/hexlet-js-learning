11daa33aedae42b0e1767966aaf693ce89c94c3b
Constantine Anikin <mail@anikin.pro>
2024-02-29 12:47:49 -0500

    Update strings.js

--- arrays/strings.js
+++ arrays/strings.js
@@ -68,3 +68,36 @@ const makeCensored = (sentence, stopList) => {
 };
 
 export default makeCensored;
+
+
+// Реализуйте и экспортируйте по умолчанию функцию, которая получает на вход строку и считает, 
+// сколько символов (без учёта повторяющихся символов) использовано в этой строке. 
+// Например, в строке yy используется всего один символ — y. А в строке 111yya! — используется четыре символа: 1, y, a и !:
+
+// const text1 = 'yyab'; // y, a, b
+// countUniqChars(text1); // 3
+ 
+// const text2 = 'You know nothing Jon Snow';
+// countUniqChars(text2); // 13
+ 
+// // Если передана пустая строка, то функция должна вернуть `0`
+// const text3 = '';
+// countUniqChars(text3); // 0
+
+const countUniqChars = (string) => {
+  if (string.length === 0) {
+    return 0;
+  }
+
+  const result = [];
+
+  for (const char of string) {
+    if (!result.includes(char)) {
+      result.push(char);
+    }
+  }
+
+  return result.length;
+};
+
+export default countUniqChars;
