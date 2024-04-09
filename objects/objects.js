21f2f78b33ae79faea5071c35a9baf37d97fba44
Constantine Anikin <mail@anikin.pro>
2024-04-09 16:00:22 -0400

    Update objects.js

--- objects/objects.js
+++ objects/objects.js
@@ -176,3 +176,39 @@ export default (text) => {
 
   return wordCounts;
 };
+
+
+// Реализуйте и экспортируйте функцию по умолчанию, которая формирует из переданного объекта другой объект, 
+// включающий только указанные свойства. 
+
+// Параметры:
+
+// Исходный объект
+// Массив имен свойств
+// import pick from './objects.js';
+
+// const data = {
+//   user: 'ubuntu',
+//   cores: 4,
+//   os: 'linux',
+// };
+
+// pick(data, ['user']); // { user: 'ubuntu' }
+// pick(data, ['user', 'os']); // { user: 'ubuntu', os: 'linux' }
+// pick(data, []); // {}
+// // Если такого свойства нет в исходных данных,
+// // то оно игнорируется 
+// pick(data, ['none', 'cores']); // { cores: 4 }
+
+export default (data, allowed) => {
+  const keys = Object.keys(data);
+  const result = {};
+
+  for (const key of keys) {
+    if (allowed.includes(key)) {
+      result[key] = data[key];
+    }
+  }
+
+  return result;
+};
