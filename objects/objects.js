cdd5e96fb30100601381b58ec3aae726d73bea7f
Constantine Anikin <mail@anikin.pro>
2024-04-12 12:00:39 -0400

    Update objects.js

--- objects/objects.js
+++ objects/objects.js
@@ -314,3 +314,35 @@ export default (object, keys, data) => {
 
   return object;
 };
+
+
+// Реализуйте и экспортируйте по умолчанию функцию, которая выполняет поверхностное копирование объектов.
+
+// import cloneShallow from '../objects.js';
+
+// const data = {
+//   key: 'value',
+//   key2: {
+//     key: 'innerValue',
+//     innerKey: {
+//       anotherKey: 'anotherValue',
+//     },
+//   },
+// };
+
+// // result имеет такую же структуру, как и data
+// const result = cloneShallow(data);
+// Реализуйте этот функционал самостоятельно, не используя функцию Object.assign().
+
+// Подсказки
+// Для решения этой задачи, нужно последовательно обойти исходный объект и скопировать его данные в другой объект.
+
+export default (data) => {
+  const result = {};
+
+  for (const key in data) {
+    result[key] = data[key];
+  }
+
+  return result;
+};
