72c8cbe801e58ad74f01fad2a443665df328ba5b
Constantine Anikin <mail@anikin.pro>
2024-04-12 10:24:57 -0400

    Update objects.js

--- objects/objects.js
+++ objects/objects.js
@@ -212,3 +212,60 @@ export default (data, allowed) => {
 
   return result;
 };
+
+
+// Реализуйте и экспортируйте по умолчанию функцию, которая извлекает из объекта любой глубины вложенности значение по указанным ключам. 
+
+// Параметры:
+// Исходный объект
+// Цепочка ключей (массив), по которой ведётся поиск значения
+// В случае, когда добраться до значения невозможно, возвращается null.
+
+// import get from './objects.js';
+
+// const data = {
+//   user: 'ubuntu',
+//   hosts: {
+//     0: {
+//       name: 'web1',
+//     },
+//     1: {
+//       name: 'web2',
+//       null: 3,
+//       active: false,
+//     },
+//   },
+// };
+
+// get(data, ['undefined']); // null
+// get(data, ['user']); // 'ubuntu'
+// get(data, ['user', 'ubuntu']); // null
+// get(data, ['hosts', 1, 'name']); // 'web2'
+// get(data, ['hosts', 0]); // { name: 'web1' }
+// get(data, ['hosts', 1, null]); // 3
+// get(data, ['hosts', 1, 'active']); // false
+// get(data, []); // { user: 'ubuntu', hosts: { 0: { name: 'web1' }, 1: { name: 'web2', null: 3, active: false }}}
+// В этой задаче нельзя использовать библиотеку lodash. Смысл задачи в том, чтобы реализовать всё самостоятельно.
+
+// Для проверки наличия ключа используйте вариант предлагаемый ниже:
+
+// // Метод Object.hasOwn() проверяет есть ли в объекте ключ,
+// // просто скопируйте этот вызов и подставьте правильные аргументы
+
+// const obj = { key: 'value' };
+// Object.hasOwn(obj, 'key'); // true
+// Object.hasOwn(obj, 'another key'); // false
+
+export default (object, keys) => {
+  let result = object;
+
+  for (const key of keys) {
+    if (Object.hasOwn(result, key)) {
+      result = result[key];
+    } else {
+      return null;
+    }
+  }
+
+  return result;
+};
